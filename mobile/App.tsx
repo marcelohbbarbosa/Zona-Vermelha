66859681d0ff0e6d7842e962bf2c02179bcf2462
S1lva08 <frostn0708@gmail.com>
2026-05-20 23:24:07 -0300

    Commit 1
    Adição inicial da tela de login

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,6 @@
-import { View, Text } from "react-native";
+import Login from "./src/screens/login";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Zona Vermelha TCC</Text>
-    </View>
-  );
+
+  return <Login />;
 }
